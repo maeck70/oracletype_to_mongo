5c5f869f815e8acaf2c6ffb0b7dfe7c7c109d020
Marcel van Eck <vaneck.marcel@gmail.com>
2024-06-06 07:45:20 -0700

    added types

--- oracle2mongo_test.go
+++ oracle2mongo_test.go
@@ -11,16 +11,19 @@ func TestXlat(t *testing.T) {
 		oracleType    string
 		MongoExpected string
 	}{
-		{"NUMBER[19,5]", "float"},
+		{"NUMBER[19,5]", "decimal"},
 		{"VARCHAR2", "string"},
 		{"NUMBER[1,0]", "bool"},
 		{"NUMBER[2,0]", "int"},
-		{"NUMBER[2,2]", "float"},
+		{"NUMBER[2,2]", "decimal"},
 		{"NUMBER[19,18]", "decimal"},
 		{"NUMBER[38,19]", "decimal"},
 		{"NUMBER[0,0]", "int"},
 		{"FLOAT", "float"},
 		{"NUMBER[5,10]", "unknown"},
+		{"DATE", "date"},
+		{"TIMESTAMP(4)", "timestamp"},
+		{"INT64", "long"},
 	}
 
 	type result_t struct {
--- xlat_table.go
+++ xlat_table.go
@@ -9,6 +9,7 @@ var xlatTable = []xlat_t{
 	{"NVARCHAR", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "string"},
 	{"NVARCHAR2", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "string"},
 	{"CLOB", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "string"},
+	{"NCLOB", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "string"},
 	{"BLOB", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "binary data"},
 	{"BOOL", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "bool"},
 	{"NUMBER", num_t{scale: lowhigh_t{1, 1}, precision: lowhigh_t{0, 0}}, "bool"},
@@ -16,4 +17,7 @@ var xlatTable = []xlat_t{
 	{"NUMBER", num_t{scale: lowhigh_t{1, 38}, precision: lowhigh_t{1, 38}}, "decimal"},
 	{"NUMBER", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "int"},
 	{"FLOAT", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "float"},
+	{"DATE", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "date"},
+	{"TIMESTAMP(4)", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "timestamp"},
+	{"INT64", num_t{scale: lowhigh_t{0, 0}, precision: lowhigh_t{0, 0}}, "long"},
 }
